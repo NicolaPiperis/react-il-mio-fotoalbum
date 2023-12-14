31747f0fd0f28bcb3fcaa3e41b8732ba544615c7
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-12-14 11:59:15 +0100

    show photo

--- controllers/photos.js
+++ controllers/photos.js
@@ -1,6 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+// Crea una colonna foto
 async function store (req, res) {
     const photoData = req.body;
     const newPhoto = await prisma.photo.create({
@@ -15,14 +16,26 @@ async function store (req, res) {
     return res.json(newPhoto);
 };
 
+// Visualizza le colonne foto
 async function index (req, res) {
     const data = await prisma.photo.findMany();
-    
-    res.json(data);
+
+    return res.json(data);
 };
 
+// Visualizza la colonna foto desiderata, attraverso un parametro(id)
 async function show (req, res) {
-
+    const idParams = req.params.id;
+    const data = await prisma.photo.findUnique({
+        where: {
+            id: parseInt(idParams)
+        }
+    })
+    if(!data) {
+        res.send("Photo non trovata")
+    }
+    
+    return res.json(data);
 };
 
 async function update(req, res) {
